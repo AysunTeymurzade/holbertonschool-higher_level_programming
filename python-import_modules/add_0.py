815c95e903f192675313779437f7646b56c5ea2b
AysunTeymurzade <aysunteymurzade0409@gmail.com>
2025-05-01 12:12:50 +0000

    mytask0

--- python-import_modules/add_0.py
+++ python-import_modules/add_0.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 def add(a, b):
-    """My addition function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a + b
-    """
+    """My addition function"""
     return (a + b)
+
+if __name__ == "__main__":
+    a = 1  # a
+    b = 2  # b
+    result = add(a, b)
+    print(f"{a} + {b} = {result}")
