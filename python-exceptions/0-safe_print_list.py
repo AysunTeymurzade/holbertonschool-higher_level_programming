66cd7215e5e8a5ccbd05512c0bee1de150061bd4
AysunTeymurzade <aysunteymurzade0409@gmail.com>
2025-05-05 12:11:28 +0000

    mytask0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     count = 0
-    try:
-        for i in range(x):
-            print(my_list[i], end='')  # Print elements without a newline after each
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
             count += 1
-    except IndexError:
-        pass  # If there is an IndexError, just stop printing
-
-    print()  # Print a newline at the end
+        except IndexError:
+            break
+    print()  # Print a new line after the elements
     return count
